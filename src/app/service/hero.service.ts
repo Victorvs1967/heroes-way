bb25aa42eb16d1652429597135754e7d58921384
Victors1967 <victorsmirnov@me.com>
2021-04-13 22:36:59 +0300

    edit search

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Hero} from '../data/hero';
+import { Hero } from '../data/hero';
 import { MessageService } from './message.service';
 
 const httpOptions = {
@@ -36,11 +36,24 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
       tap(_ => this.log('fetched heroes')),
-      catchError(this.handleError<Hero[]>('getHeroes', [])));
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+    .pipe(
+      map(heroes => heroes[0]),
+      tap(h => {
+        const outcome = h ? 'fetched' : 'did not find';
+        this.log(`${outcome} hero id=${id}`);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 
   getHero(id: number): Observable<Hero> {
@@ -84,7 +97,7 @@ export class HeroService {
     if (!term.trim()) {
       return of([]);
     }
-    return this.http.get<Hero[]>(`${this.heroesUrl}/?name${term}`)
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
     .pipe(
       tap(_ => this.log(`found heroes matching ${term}`)),
       catchError(this.handleError<Hero[]>('searchHeroes', []))
